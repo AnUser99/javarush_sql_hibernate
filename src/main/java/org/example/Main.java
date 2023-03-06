5651901ace1369ca18be39f4f225ce81a25af49d
AnUser99 <kisamasyanya@gmail.com>
2023-03-06 10:21:16 +0300

    7_2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,6 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,27 +16,47 @@ public class Main {
              Statement statement = connection.createStatement();
         ) {
             ResultSet results = statement.executeQuery(query1 + table1);
+            showData(results);
+            System.out.println();
+            ResultSetMetaData metaData = results.getMetaData();
+            showMetaData(metaData);
 
-            while (results.next()) {
-                int id = results.getInt(1);
-                String name = results.getString(2);
-                String surname = results.getString(3);
-                String department = results.getString(4);
-                int salary = results.getInt(5);
-                int row = results.getRow();
-
-                System.out.println(
-                        row + "\t" +
-                        id + "\t" +
-                        name + "\t" +
-                        surname + "\t" +
-                        department + "\t" +
-                        salary
-                );
-            }
         } catch (SQLException e) {
             System.out.println("Exception " + e);
 //            throw new RuntimeException(e);
+        }
+    }
+    
+    public static void showData(ResultSet results) throws SQLException {
+        while (results.next()) {
+            int id = results.getInt(1);
+            String name = results.getString(2);
+            String surname = results.getString(3);
+            String department = results.getString(4);
+            int salary = results.getInt(5);
+            int row = results.getRow();
+
+            System.out.println(
+                    row + " " +
+                            id + " " +
+                            name + " " +
+                            surname + " " +
+                            department + " " +
+                            salary
+            );
+        }
+    }
+
+    public static void showMetaData(ResultSetMetaData metaData) throws SQLException {
+        int columnCount = metaData.getColumnCount();
+        for (int column = 1; column <= columnCount; column++)
+        {
+            String name = metaData.getColumnName(column);
+            String className = metaData.getColumnClassName(column);
+            String typeName = metaData.getColumnTypeName(column);
+            int type = metaData.getColumnType(column);
+
+            System.out.println(name + " " + className + " " + typeName + " " + type);
         }
     }
 }
